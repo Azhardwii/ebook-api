b71591c90a30f5fead330c73e37667847d0ce6fb
Azhardwii <halloazhardwii@gmail.com>
2022-10-26 11:05:50 +0700

    routing + controller + model + migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controller\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('halo', function(){
+    return ["me" => "Guanteng"];
 });
+
+// Route::get('halcontroller', [HeloController::class, 'index']);
+// Route::post('halcontroller', [HeloController::class, 'store']);
+// Route::get('halcontroller', [HeloController::class, 'show']);
+// Route::put('halcontroller', [HeloController::class, 'update']);
+// Route::delete('halcontroller', [HeloController::class, 'destroy']);
+Route::resource('halcontroller', HeloController::class);
+
+Route::resource('siswa', SiswaController::class);
